cf2074aac03ee335cb768b797413f7b179dc2658
Fatine EL HARIRI <hariri.fatine17@gmail.com>
2022-06-29 23:45:08 +0100

    restructuring the application

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -7,19 +7,7 @@ import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
 
-    apiKey: "AIzaSyDFImCUb5_mflcIpkLsQwlA_vs26hbMV14",
-  
-    authDomain: "chatapp-8f796.firebaseapp.com",
-  
-    projectId: "chatapp-8f796",
-  
-    storageBucket: "chatapp-8f796.appspot.com",
-  
-    messagingSenderId: "1028289582412",
-  
-    appId: "1:1028289582412:web:e5d0396482002f3c256be9",
-  
-    measurementId: "G-ZWKGD68BB3"
+    //usr
   
   
   });
