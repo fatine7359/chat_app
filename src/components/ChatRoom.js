298011ed6a421de15f17769a88da055910c324f3
Fatine EL HARIRI <hariri.fatine17@gmail.com>
2022-06-30 19:22:03 +0100

    adding credientials

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -7,7 +7,19 @@ import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
 
-    //usr
+    apiKey: "AIzaSyDFImCUb5_mflcIpkLsQwlA_vs26hbMV14",
+  
+    authDomain: "chatapp-8f796.firebaseapp.com",
+  
+    projectId: "chatapp-8f796",
+  
+    storageBucket: "chatapp-8f796.appspot.com",
+  
+    messagingSenderId: "1028289582412",
+  
+    appId: "1:1028289582412:web:e5d0396482002f3c256be9",
+  
+    measurementId: "G-ZWKGD68BB3"
   
   
   });
@@ -44,7 +56,7 @@ export const ChatRoom = () => {
       <>
         <main>
           { messages && messages.map(message => <ChatMessage key={message.id} message={message}/>)}
-  
+          
           <span ref={dummy}></span>
   
         </main>
--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -6,7 +6,19 @@ import 'firebase/compat/auth';
 
 firebase.initializeApp({
 
-    //urs
+    apiKey: "AIzaSyDFImCUb5_mflcIpkLsQwlA_vs26hbMV14",
+  
+    authDomain: "chatapp-8f796.firebaseapp.com",
+  
+    projectId: "chatapp-8f796",
+  
+    storageBucket: "chatapp-8f796.appspot.com",
+  
+    messagingSenderId: "1028289582412",
+  
+    appId: "1:1028289582412:web:e5d0396482002f3c256be9",
+  
+    measurementId: "G-ZWKGD68BB3"
   
   
   });
